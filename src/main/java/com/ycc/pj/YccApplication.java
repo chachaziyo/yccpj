6f528dbea0798d1f4c2214db8548cddef1933f62
chachaziyo <chachaziyo@gmail.com>
2023-10-01 12:10:03 +0900

    최초프로젝트

---
+++ src/main/java/com/ycc/pj/YccApplication.java
@@ -0,0 +1,13 @@
+package com.ycc.pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ycc/pj/YccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycc.pj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
